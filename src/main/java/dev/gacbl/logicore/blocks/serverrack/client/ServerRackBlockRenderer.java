12b246250c7f21093a0b86de40a77a475fd10066
Igor Ilic <github@igorilic.net>
2026-05-16 19:01:17 +0200

    Porting to 26.1

--- src/main/java/dev/gacbl/logicore/blocks/serverrack/client/ServerRackBlockRenderer.java
+++ src/main/java/dev/gacbl/logicore/blocks/serverrack/client/ServerRackBlockRenderer.java
@@ -6,20 +6,22 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.resources.Identifier;
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
-public class ServerRackBlockRenderer extends GeoBlockRenderer<ServerRackBlockEntity, ServerRackRenderState> {
+//
+public class ServerRackBlockRenderer extends GeoBlockRenderer<ServerRackBlockEntity, @NonNull ServerRackRenderState> {
     public ServerRackBlockRenderer(BlockEntityRendererProvider.Context context) {
         super(context, new ServerRackBlockModel());
     }
 
     @Override
-    public ServerRackRenderState createRenderState() {
+    public @NonNull ServerRackRenderState createRenderState() {
         return new ServerRackRenderState();
     }
 
     @Override
-    public @Nullable RenderType getRenderType(ServerRackRenderState renderState, Identifier texture) {
+    public @Nullable RenderType getRenderType(@NonNull ServerRackRenderState renderState, @NonNull Identifier texture) {
         return RenderTypes.entityCutoutCull(texture);
     }
 }
--- src/main/java/dev/gacbl/logicore/data/ModModelProvider.java
+++ src/main/java/dev/gacbl/logicore/data/ModModelProvider.java
@@ -16,7 +16,6 @@ import dev.gacbl.logicore.blocks.serverrack.ServerRackModule;
 import dev.gacbl.logicore.items.knowledge_orb.KnowledgeOrbModule;
 import dev.gacbl.logicore.items.pickaxe.CyclePickModule;
 import dev.gacbl.logicore.items.processorunit.ProcessorUnitModule;
-import dev.gacbl.logicore.items.server.ServerModule;
 import dev.gacbl.logicore.items.stack_upgrade.StackUpgradeModule;
 import dev.gacbl.logicore.items.wrench.WrenchModule;
 import net.minecraft.client.data.models.BlockModelGenerators;
@@ -39,30 +38,20 @@ public class ModModelProvider extends ModelProvider {
         super(output, LogiCore.MOD_ID);
     }
 
-    private ItemModelGenerators itemModels = null;
-    private BlockModelGenerators blockModels = null;
-
     @Override
     protected @NonNull Stream<? extends Holder<Block>> getKnownBlocks() {
-        List<Holder<Block>> list = new ArrayList<>();
-        list.addAll(BatteryModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(CloudInterfaceModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(CompilerModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(ComputerModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(DataCableModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(DatacenterModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(DatacenterPortModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(GeneratorModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(RecyclerModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(RepairStationModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(ResearchStationModule.BLOCKS.getEntries().stream().toList());
-        list.addAll(ServerRackModule.BLOCKS.getEntries().stream().toList());
-        return list.stream();
+        return Stream.empty();
     }
 
     @Override
     protected @NonNull Stream<? extends Holder<Item>> getKnownItems() {
         List<Holder<Item>> list = new ArrayList<>();
+
+        list.addAll(ProcessorUnitModule.ITEMS.getEntries().stream().toList());
+        list.addAll(WrenchModule.ITEMS.getEntries().stream().toList());
+        list.addAll(StackUpgradeModule.ITEMS.getEntries().stream().toList());
+        list.addAll(CyclePickModule.ITEMS.getEntries().stream().toList());
+        list.addAll(KnowledgeOrbModule.ITEMS.getEntries().stream().toList());
 
         list.addAll(BatteryModule.ITEMS.getEntries().stream().toList());
         list.addAll(CloudInterfaceModule.ITEMS.getEntries().stream().toList());
@@ -82,9 +71,6 @@ public class ModModelProvider extends ModelProvider {
 
     @Override
     protected void registerModels(@NonNull BlockModelGenerators blockModels, @NonNull ItemModelGenerators itemModels) {
-        this.blockModels = blockModels;
-        this.itemModels = itemModels;
-
         itemModels.generateFlatItem(ProcessorUnitModule.PROCESSOR_UNIT_BASIC.get(), ModelTemplates.FLAT_ITEM);
         itemModels.generateFlatItem(ProcessorUnitModule.PROCESSOR_UNIT_ADVANCE.get(), ModelTemplates.FLAT_ITEM);
         itemModels.generateFlatItem(ProcessorUnitModule.PROCESSOR_UNIT_ULTIMATE.get(), ModelTemplates.FLAT_ITEM);
@@ -92,22 +78,5 @@ public class ModModelProvider extends ModelProvider {
         itemModels.generateFlatItem(StackUpgradeModule.STACK_UPGRADE.get(), ModelTemplates.FLAT_ITEM);
         itemModels.generateFlatItem(CyclePickModule.CYCLE_PICK.get(), ModelTemplates.FLAT_HANDHELD_ITEM);
         itemModels.generateFlatItem(KnowledgeOrbModule.KNOWLEDGE_ORB.get(), ModelTemplates.FLAT_ITEM);
-        itemModels.generateFlatItem(ServerModule.SERVER.get(), ModelTemplates.FLAT_ITEM);
-
-        /*blockModels.createFlatItemModelWithBlockTexture(CompilerModule.COMPILER_ITEM.get(), CompilerModule.COMPILER_BLOCK.get());
-        blockModels.createFlatItemModelWithBlockTexture(ComputerModule.COMPUTER_ITEM.get(), ComputerModule.COMPUTER_BLOCK.get());
-        blockModels.createFlatItemModelWithBlockTexture(DataCableModule.DATA_CABLE_ITEM.get(), DataCableModule.DATA_CABLE_BLOCK.get());
-        blockModels.createFlatItemModelWithBlockTexture(DatacenterModule.DATACENTER_CONTROLLER_ITEM.get(), DatacenterModule.DATACENTER_CONTROLLER.get());
-        blockModels.createFlatItemModelWithBlockTexture(DatacenterPortModule.DATACENTER_PORT_ITEM.get(), DatacenterPortModule.DATACENTER_PORT.get());
-        blockModels.createFlatItemModelWithBlockTexture(GeneratorModule.GENERATOR_ITEM.get(), GeneratorModule.GENERATOR.get());
-        blockModels.createFlatItemModelWithBlockTexture(CloudInterfaceModule.CLOUD_INTERFACE_ITEM.get(), CloudInterfaceModule.CLOUD_INTERFACE.get());
-        blockModels.createFlatItemModelWithBlockTexture(ResearchStationModule.RESEARCH_STATION_ITEM.get(), ResearchStationModule.RESEARCH_STATION.get());
-        blockModels.createFlatItemModelWithBlockTexture(RepairStationModule.REPAIR_STATION_ITEM.get(), RepairStationModule.REPAIR_STATION.get());
-        blockModels.createFlatItemModelWithBlockTexture(RecyclerModule.RECYCLER_ITEM.get(), RecyclerModule.RECYCLER.get());
-        blockModels.createFlatItemModelWithBlockTexture(ServerRackModule.SERVER_RACK_ITEM.get(), ServerRackModule.SERVER_RACK.get());
-        blockModels.createFlatItemModelWithBlockTexture(BatteryModule.BATTERY_CREATIVE.asItem(), BatteryModule.BATTERY_CREATIVE.get());
-        blockModels.createFlatItemModelWithBlockTexture(BatteryModule.BATTERY_LARGE.asItem(), BatteryModule.BATTERY_LARGE.get());
-        blockModels.createFlatItemModelWithBlockTexture(BatteryModule.BATTERY_MEDIUM.asItem(), BatteryModule.BATTERY_MEDIUM.get());
-        blockModels.createFlatItemModelWithBlockTexture(BatteryModule.BATTERY_SMALL.asItem(), BatteryModule.BATTERY_SMALL.get());*/
     }
 }
